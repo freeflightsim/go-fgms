0e3cc52b053f40b59ca87244f99de7d5db6717ad
Pedro <pete@daffodil.uk.com>
2014-10-12 10:15:39 +0100

    add docs

---
+++ message/doc.go
@@ -0,0 +1,9 @@
+/*
+Package message - Message definitions and XDR decoding
+
+
+	This is ported from
+	 - http://gitorious.org/fgms/fgms-0-x/blobs/master/src/flightgear/MultiPlayer/mpmessages.hxx
+
+*/
+package message
